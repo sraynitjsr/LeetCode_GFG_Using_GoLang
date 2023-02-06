9019a50d360a38f5d7f52ae1c72bc8a19266326a
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-02-06 23:45:55 +0530

    Printing Maximum Sum Sub Array With Maximum Sum

--- 10-maximum-sum-sub-array.go
+++ 10-maximum-sum-sub-array.go
@@ -5,18 +5,33 @@ import (
 	"math"
 )
 
-func startTen() {
-	mySlice := []int{-2, -3, 4, -1, -2, 1, 5, -3}
+func printMaximumSumSubArray(mySlice []int) {
 	maxSoFar := math.MinInt
 	maxTillNow := 0
+	start := 0
+	end := 0
+	currentStart := 0
 	for i := 0; i < len(mySlice); i++ {
 		maxTillNow = maxTillNow + mySlice[i]
-		if maxTillNow > maxSoFar {
+		if maxSoFar < maxTillNow {
 			maxSoFar = maxTillNow
+			start = currentStart
+			end = i
 		}
 		if maxTillNow < 0 {
 			maxTillNow = 0
+			currentStart = i + 1
 		}
 	}
-	fmt.Println("Maximum Sum => ", maxSoFar)
+	fmt.Println("Maximum Sum =>", maxSoFar)
+	fmt.Print("Maximum Sum Sub Array is => ")
+	for i := start; i <= end; i++ {
+		fmt.Print(mySlice[i], " ")
+	}
+	fmt.Println("")
+}
+
+func startTen() {
+	mySlice := []int{-2, -3, 4, -1, -2, 1, 5, -3}
+	printMaximumSumSubArray(mySlice)
 }
