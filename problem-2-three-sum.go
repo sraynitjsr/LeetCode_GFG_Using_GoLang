1f92452e2b10bb85ef17952615ad65d5a8f82ecd
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-05-17 04:52:11 +0530

    Triplet Found Using Hashing

--- problem-2-three-sum.go
+++ problem-2-three-sum.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
+func hashingSolution(slice []int, target int) {
+	fmt.Print("Using Hashing, ")
+
+	myMap := make(map[int]int)
+	for i := 0; i < len(slice); i++ {
+		myMap[slice[i]] = i
+	}
+
+	for i := 0; i < len(slice); i++ {
+		for j := i + 1; j < len(slice); j++ {
+			index, present := myMap[target-slice[i]-slice[j]]
+			if present {
+				fmt.Println("Found Triplet =>", slice[index], slice[i], slice[j])
+				return
+			}
+		}
+	}
+	fmt.Println("No Triplets Found")
+}
+
+func sortingSolution(slice []int, target int) {
+	fmt.Println("Using Sorting")
+}
+
 func threeSum() {
 	fmt.Println("Three Sum Array")
+	mySlice := []int{1, 4, 5, 2, 3}
+	target := 8
+	hashingSolution(mySlice, target)
+	sortingSolution(mySlice, target)
 }
