8566c0c04c0f5796d20b9934e4ce7e2bbe689ca1
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-08-04 01:57:27 +0530

    Missing Number in GoLang

--- main.go
+++ main.go
@@ -7,10 +7,7 @@ import (
 
 func main() {
 	fmt.Println("LeetCode and DSA Using GoLang")
-
 	
 	intSlice := []int{2, 5, 3, 6, 4}
 	fmt.Println("Checking for missing number is =>", blind75.MissingNumber(intSlice))
-
-	fmt.Println("Checking for duplicetae keys in an array", blind75.ContainsDuplicate())
 }
