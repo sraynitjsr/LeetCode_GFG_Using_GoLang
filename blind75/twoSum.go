0aa45d4156ef86899d8d6858aeb05ce0e71999ae
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-08-15 20:25:34 +0530

    Pair Sum Problem

--- blind75/twoSum.go
+++ blind75/twoSum.go
@@ -1,1 +1,19 @@
+package blind75
 
+import "fmt"
+
+func TwoSum(mySlice []int, target int) []int {
+    helperValueIndexMap := make(map[int]int)
+    output := []int{}
+    for index,element := range mySlice {
+        anotherIndex, presentOrNot := helperValueIndexMap[target-element]
+        if !presentOrNot {
+            helperValueIndexMap[element] = index
+        } else {
+            output = append(output, index)
+            output = append(output, anotherIndex)
+            break
+        }
+    }
+    return output
+}
