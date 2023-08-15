0fbaa212cdd29fa19a8f4a1bf5079f392e54155a
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-08-15 20:26:57 +0530

    Finding Pair With Given Sum

--- main.go
+++ main.go
@@ -9,5 +9,8 @@ func main() {
 	fmt.Println("LeetCode and DSA Using GoLang")
 	
 	intSlice := []int{2, 5, 3, 6, 4}
+	
 	fmt.Println("Checking for missing number is =>", blind75.MissingNumber(intSlice))
+
+	fmt.Println("Two Sum Pair =>", blind75.TwoSum(intSlice, 11))
 }
