180c4d82473206502f434af03a08e5fed926afc2
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-01-26 12:18:18 +0530

    Two Sum Problem

--- Blind75/main.go
+++ Blind75/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	blind75 "github.com/sraynitjsr/blind75"
+)
 
 func main() {
 	fmt.Println("This is blind75 DSA question solutions repo")
+
+	fmt.Println("\nTwo Sum Problem")
+	blind75.Start()
 }
