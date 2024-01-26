5b53abf38518ec99caae63156ac19b23e02c4ad2
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-01-26 12:21:30 +0530

    Finding Pair for Given Sum in An Array

---
+++ blind75/1twoSum.go
@@ -0,0 +1,29 @@
+package blind75
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	numMap := make(map[int]int)
+
+	for i, num := range nums {
+		complement := target - num
+		if index, ok := numMap[complement]; ok {
+			return []int{index, i}
+		}
+		numMap[num] = i
+	}
+
+	return nil
+}
+
+func Start() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+
+	result := twoSum(nums, target)
+	if result != nil {
+		fmt.Printf("Two numbers at indices %v and %v add up to %v\n", result[0], result[1], target)
+	} else {
+		fmt.Println("No such pair found.")
+	}
+}
