df2d92c207a897cb4bd19ee0e07da0ca51059aaa
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-01-28 03:06:56 +0530

    Finding duplicate elements in an array

--- blind75/3containsDuplicate.go
+++ blind75/3containsDuplicate.go
@@ -1,1 +1,29 @@
+package blind75
 
+import "fmt"
+
+func findDuplicates(arr []int) []int {
+	duplicates := make([]int, 0)
+	visited := make(map[int]bool)
+
+	for _, num := range arr {
+		if visited[num] {
+			duplicates = append(duplicates, num)
+		} else {
+			visited[num] = true
+		}
+	}
+	return duplicates
+}
+
+func ContainsDuplicate () {
+	array := []int{1, 2, 3, 4, 5, 2, 7, 8, 5, 1}
+
+	duplicates := findDuplicates(array)
+
+	if len(duplicates) > 0 {
+		fmt.Println("Duplicate elements:", duplicates)
+	} else {
+		fmt.Println("No duplicate elements found.")
+	}
+}
